65f45c33fc68dd31842657f1ace97a4e4e02a0ca
leonardof02 <leonardoarencibia2002@gmail.com>
2023-05-12 23:47:41 -0400

    Loader of routes separated

---
+++ src/helpers/loaders/countryLoader.ts
@@ -0,0 +1,20 @@
+import { LoaderFunctionArgs } from "react-router-dom";
+import { Country } from "../Interfaces";
+
+interface CountryParams extends Record<string, string> {
+    countryName: string;
+}
+
+export async function countryLoader({ params }: LoaderFunctionArgs): Promise<Country> {
+    const countryName = (params as CountryParams).countryName;
+    const response = await fetch(
+        `https://restcountries.com/v3.1/name/${countryName
+            .toLocaleLowerCase()
+            .replace(
+                /\s+/g,
+                "%20"
+            )}?fields=name,population,region,subregion,capital,flags,tld,currencies,languages,borders`
+    );
+    const country: Country = await response.json();
+    return country;
+}
