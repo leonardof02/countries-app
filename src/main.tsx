f72b5dc8cf6cab1735f0428c6e16c6c00b60862a
leonardof02 <leonardoarencibia2002@gmail.com>
2023-05-14 18:50:36 -0400

    Router fixed for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./styles/_global.scss";
 import CountryInfoScreen from "./components/CountryInfoScreen.tsx";
@@ -8,7 +8,7 @@ import CountriesMenuScreen from "./components/CountriesMenuScreen.tsx";
 
 import { countryLoader } from "./helpers/loaders/countryLoader.ts";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App />,
